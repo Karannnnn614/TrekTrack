94eb7da2eb8dba791fe431f7fa19e0ff3f388ff1
Karan Mundre <karanmundre73@gmail.com>
2025-04-12 00:04:51 +0530

    changed API_URL

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Project } from "../types";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
